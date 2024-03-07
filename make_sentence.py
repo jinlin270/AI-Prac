b561c7e7ef012c0456d8d346a637a2bc0c550846
jinlin270 <jinlin270@gmail.com>
2024-03-06 22:59:11 -0500

    fix how pre-processing to correct generated csv, fix make_sentence to correctly extract 10 random image vectors

--- make_sentence.py
+++ make_sentence.py
@@ -1,1 +1,21 @@
 import numpy as np
+
+# Define the number of lines to extract
+num_lines = 10
+
+# Count the total number of lines in the CSV file
+num_lines_total = sum(1 for _ in open("ETL1_n.csv"))
+
+# Generate random line numbers to extract
+random_lines = np.random.choice(num_lines_total, num_lines, replace=False)
+
+# Read the selected lines from the CSV file using numpy
+selected_data = np.genfromtxt(
+    "ETL1_n.csv",
+    delimiter=",",
+    skip_header=random_lines[0],
+    max_rows=num_lines,
+)
+
+# Display the selected data
+print(selected_data)
--- pre-process.py
+++ pre-process.py
@@ -2,17 +2,16 @@ import numpy as np
 import cv2
 import os
 
-IMG_DIR = "/Users/linjin/Downloads/dataset/filtered/ETL6"
-
-for subdir in os.listdir(IMG_DIR):
-    subdir_path = os.path.join(IMG_DIR, subdir)
-    if os.path.isdir(subdir_path):
-        for file_name in os.listdir(subdir_path):
-            file_path = os.path.join(subdir_path, file_name)
-            img_array = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
-            if img_array is not None:
-                img_array = img_array.flatten()
-                img_array = img_array.reshape(-1, 1).T
-                print(img_array)
-                with open("ETL6.csv", "ab") as f:
-                    np.savetxt(f, img_array, delimiter=",")
+IMG_DIR = "/Users/linjin/Desktop/AI Prac/dataset/filtered/ETL1"
+with open("ETL1_n.csv", "wb") as f:
+    for subdir in os.listdir(IMG_DIR):
+        subdir_path = os.path.join(IMG_DIR, subdir)
+        if os.path.isdir(subdir_path):
+            for file_name in os.listdir(subdir_path):
+                file_path = os.path.join(subdir_path, file_name)
+                img_array = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+                if img_array is not None:
+                    img_array = img_array.flatten()
+                    img_array = img_array.reshape(-1, 1).T
+                    print(img_array)
+                    np.savetxt(f, img_array, delimiter=",", fmt="%d", newline="\n")
